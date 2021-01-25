f48464265c2c774c6616cbda1c96e2f7591dd7ae
Dell Latitude E5530 <pfewfw@wp.pl>
2021-01-25 13:59:21 +0100

    First configuration of the project

---
+++ src/main/java/com/radello/glasses_store/GlassesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.radello.glasses_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassesStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlassesStoreApplication.class, args);
+    }
+
+}
