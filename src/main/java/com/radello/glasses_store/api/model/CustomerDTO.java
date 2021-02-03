df8bf4f8e85a17930c918d20fb693bce47e4620c
Dell Latitude E5530 <pfewfw@wp.pl>
2021-02-03 13:03:21 +0100

    Customizing Swagger (Api Model Property)

--- src/main/java/com/radello/glasses_store/api/model/CustomerDTO.java
+++ src/main/java/com/radello/glasses_store/api/model/CustomerDTO.java
@@ -1,5 +1,6 @@
 package com.radello.glasses_store.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -10,10 +11,16 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerDTO {
+    @ApiModelProperty(value =" Customers Id")
     private Long ID;
+    @ApiModelProperty(value =" Customers name")
     private String name;
+    @ApiModelProperty(value =" Customers surname")
     private String surname;
+    @ApiModelProperty(value =" Customers telephone")
     private int telephone;
+    @ApiModelProperty(value =" Customers city")
     private String city;
+    @ApiModelProperty(value =" Customers list of glasses")
     private List<GlassesDTO> listOfGlasses = new ArrayList<>();
 }
--- src/main/java/com/radello/glasses_store/api/model/GlassesDTO.java
+++ src/main/java/com/radello/glasses_store/api/model/GlassesDTO.java
@@ -1,5 +1,6 @@
 package com.radello.glasses_store.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
@@ -8,9 +9,14 @@ import lombok.*;
 @AllArgsConstructor
 public class GlassesDTO {
 
+    @ApiModelProperty(value =" Glasses Id")
     private Long Id;
+    @ApiModelProperty(value =" Glasses number")
     private int number;
+    @ApiModelProperty(value =" Glasses model")
     private ModelDTO model;
+    @ApiModelProperty(value =" Glasses quantity")
     private int quantity;
+    @ApiModelProperty(value =" Glasses customer")
     private CustomerDTO customer;
 }
